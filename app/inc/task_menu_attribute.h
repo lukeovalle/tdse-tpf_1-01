c12e12c316fb38fabeeb6137b95960795f05b48c
Euteo <matteo.aguilar3@gmail.com>
2026-02-04 17:58:55 -0300

    maquina de estados 20%

--- app/inc/task_menu_attribute.h
+++ app/inc/task_menu_attribute.h
@@ -53,52 +53,52 @@ extern "C" {
  * 	| Current               | Event                 |                       						| Next                  |                       							|
  * 	| State                 | (Parameters)          | [Guard]               						| State                 | Actions              								|
  * 	|=======================+=======================+=======================+=======================+=======================|====================================================
- * 	| ST_INIT				| EV_PRESS_HASH/ASTK													| ST_INIT				| Seleccionar configuración o lectura
-	| ST_INIT				| EV_PRESS_NEXT			| seleccionado configuracion					| ST_CONFIG				|
+ * 	| ST_INIT				| EV_PRESS_HASH/ASTK													| ST_INIT				| Seleccionar configuración o lectura				|
+	| ST_INIT				| EV_PRESS_NEXT			| seleccionado configuracion					| ST_CONFIG				|													|
 	| ST_INIT				| EV_PRESS_NEXT			| seleccionado lectura y Fecha valida guardada	| ST_READ				|
 	| ST_CONFIG				| EV_PRESS_HASH/ASTK	| 												| ST_CONFIG				| Seleccionar tiempo, temperatura, humedad o luz
-	| ST_CONFIG				| EV_PRESS_BACK		ST_INIT
-	| ST_CONFIG				| EV_PRESS_NEXT	seleccionado tiempo	ST_CONFIG_TIME
-	| ST_CONFIG				| EV_PRESS_NEXT	seleccionado temperatura	ST_CONFIG_TEMP
-	| ST_CONFIG				| EV_PRESS_NEXT	seleccionado humedad	ST_CONFIG_HUM
-	| ST_CONFIG				| EV_PRESS_NEXT	seleccionado luz	ST_CONFIG_LIG
-	| ST_CONFIG_TIME		| EV_PRESS_HASH/ASTK	Cambiar entre dia/fecha/año y hora/minuto	ST_CONFIG_TIME
-	| ST_CONFIG_TIME		| EV_PRESS_ENTER	Fecha/Horario introducido valido	ST_CONFIG_TIME	Guarda valores ingresados
-	| ST_CONFIG_TIME		| EV_PRESS_BACK		ST_CONFIG
-	| ST_CONFIG_TEMP		| EV_PRESS_HASH/ASTK	Cambiar entre maxima y minima	ST_CONFIG_TEMP
-	| ST_CONFIG_TEMP		| EV_PRESS_ENTER	Temperatura valida	ST_CONFIG_TEMP	Guarda valores ingresados
-	| ST_CONFIG_TEMP		| EV_PRESS_BACK		ST_CONFIG
-	| ST_CONFIG_HUM			| EV_PRESS_HASH/ASTK	Cambiar entre maxima y minima	ST_CONFIG_HUM
-	| ST_CONFIG_HUM			| EV_PRESS_ENTER	Humedad valida	ST_CONFIG_HUM	Guarda valores ingresados
-	| ST_CONFIG_HUM			| EV_PRESS_BACK		ST_CONFIG
-	| ST_CONFIG_LIG			| EV_PRESS_HASH/ASTK	Cambiar entre maxima y minima	ST_CONFIG_LIG
-	| ST_CONFIG_LIG			| EV_PRESS_ENTER	Luminocidad valida	ST_CONFIG_LIG	Guarda valores ingresados
-	| ST_CONFIG_LIG			| EV_PRESS_BACK		ST_CONFIG
-	| ST_READ				| EV_PRESS_HASH/ASTK		ST_READ	Seleccionar tiempo, temperatura, humedad o luz
-	| ST_READ				| EV_PRESS_BACK		ST_INIT
-	| ST_READ				| EV_PRESS_NEXT	seleccionado tiempo	ST_READ_TIME	Muestra fecha y hora actuales
-	| ST_READ				| EV_PRESS_NEXT	seleccionado temperatura	ST_READ_TEMP
-	| ST_READ				| EV_PRESS_NEXT	seleccionado humedad	ST_READ_HUM
-	| ST_READ				| EV_PRESS_NEXT	seleccionado luz	ST_READ_LIG
-	| ST_READ_TIME			| EV_PRESS_BACK		ST_READ
-	| ST_READ_TEMP			| EV_PRESS_HASH/ASTK		ST_READ_TEMP	Cambiar entre configurada e historicas
-	| ST_READ_TEMP			| EV_PRESS_NEXT	Seleccionada configurada	ST_READ_TEMP_CON	Muestra temperaturas maxima y minima
-	| ST_READ_TEMP_CON		| EV_PRESS_BACK		ST_READ_TEMP
-	| ST_READ_TEMP			| EV_PRESS_NEXT	Seleccionada historicas	ST_READ_TEMP_HIS	Muestra medición más reciente
-	| ST_READ_TEMP_HIS		| EV_PRESS_HASH/ASTK		ST_READ_TEMP_HIS	Va cambiando las mediciones mostradas
-	| ST_READ_TEMP_HIS		| EV_PRESS_BACK		ST_READ_TEMP
-	| ST_READ_HUM			| EV_PRESS_HASH/ASTK		ST_READ_HUM	Cambiar entre configurada e historicas
-	| ST_READ_HUM			| EV_PRESS_NEXT	Seleccionada configurada	ST_READ_HUM_CON	Muestra humedades maxima y minima
-	| ST_READ_HUM_CON		| EV_PRESS_BACK		ST_READ_HUM
-	| ST_READ_HUM			| EV_PRESS_NEXT	Seleccionada historicas	ST_READ_HUM_HIS	Muestra medición más reciente
-	| ST_READ_HUM_HIS		| EV_PRESS_HASH/ASTK		ST_READ_HUM_HIS	Va cambiando las mediciones mostradas
-	| ST_READ_HUM_HIS		| EV_PRESS_BACK		ST_READ_HUM
-	| ST_READ_LIG			| EV_PRESS_HASH/ASTK		ST_READ_LIG	Cambiar entre configurada e historicas
-	| ST_READ_LIG			| EV_PRESS_NEXT	Seleccionada configurada	ST_READ_LIG_CON	Muestra luminocidades maxima y minima
-	| ST_READ_LIG_CON		| EV_PRESS_BACK		ST_READ_LIG
-	| ST_READ_LIG			| EV_PRESS_NEXT	Seleccionada historicas	ST_READ_LIG_HIS	Muestra medición más reciente
-	| ST_READ_LIG_HIS		| EV_PRESS_HASH/ASTK		ST_READ_LIG_HIS	Va cambiando las mediciones mostradas
-	| ST_READ_LIG_HIS		| EV_PRESS_BACK		ST_READ_LIG
+	| ST_CONFIG				| EV_PRESS_BACK			| 												| ST_INIT				|
+	| ST_CONFIG				| EV_PRESS_NEXT			| seleccionado tiempo							| ST_CONFIG_TIME		|
+	| ST_CONFIG				| EV_PRESS_NEXT			| seleccionado temperatura						| ST_CONFIG_TEMP		|
+	| ST_CONFIG				| EV_PRESS_NEXT			| seleccionado humedad							| ST_CONFIG_HUM			|
+	| ST_CONFIG				| EV_PRESS_NEXT			| seleccionado luz								| ST_CONFIG_LIG			|
+	| ST_CONFIG_TIME		| EV_PRESS_HASH/ASTK	| 												| ST_CONFIG_TIME		| Cambiar entre dia/fecha/año y hora/minuto
+	| ST_CONFIG_TIME		| EV_PRESS_ENTER		| Fecha/Horario introducido valido				| ST_CONFIG_TIME		| Guarda valores ingresados
+	| ST_CONFIG_TIME		| EV_PRESS_BACK			| 												| ST_CONFIG				|
+	| ST_CONFIG_TEMP		| EV_PRESS_HASH/ASTK	| 												| ST_CONFIG_TEMP		| Cambiar entre maxima y minima
+	| ST_CONFIG_TEMP		| EV_PRESS_ENTER		| Temperatura valida							| ST_CONFIG_TEMP		| Guarda valores ingresados
+	| ST_CONFIG_TEMP		| EV_PRESS_BACK			|												| ST_CONFIG				|
+	| ST_CONFIG_HUM			| EV_PRESS_HASH/ASTK	|												| ST_CONFIG_HUM			| Cambiar entre maxima y minima
+	| ST_CONFIG_HUM			| EV_PRESS_ENTER		| Humedad valida								| ST_CONFIG_HUM			| Guarda valores ingresados
+	| ST_CONFIG_HUM			| EV_PRESS_BACK			|												| ST_CONFIG				|
+	| ST_CONFIG_LIG			| EV_PRESS_HASH/ASTK	|												| ST_CONFIG_LIG			| Cambiar entre maxima y minima
+	| ST_CONFIG_LIG			| EV_PRESS_ENTER		| Luminocidad valida							| ST_CONFIG_LIG			| Guarda valores ingresados
+	| ST_CONFIG_LIG			| EV_PRESS_BACK			|												| ST_CONFIG				|
+	| ST_READ				| EV_PRESS_HASH/ASTK	|												| ST_READ				| Seleccionar tiempo, temperatura, humedad o luz
+	| ST_READ				| EV_PRESS_BACK			| 												| ST_INIT				|
+	| ST_READ				| EV_PRESS_NEXT			| seleccionado tiempo							| ST_READ_TIME			| Muestra fecha y hora actuales
+	| ST_READ				| EV_PRESS_NEXT	 		| seleccionado temperatura						| ST_READ_TEMP			|
+	| ST_READ				| EV_PRESS_NEXT			| seleccionado humedad							| ST_READ_HUM			|
+	| ST_READ				| EV_PRESS_NEXT			| seleccionado luz								| ST_READ_LIG			|
+	| ST_READ_TIME			| EV_PRESS_BACK			|												| ST_READ				|
+	| ST_READ_TEMP			| EV_PRESS_HASH/ASTK	|												| ST_READ_TEMP			| Cambiar entre configurada e historicas
+	| ST_READ_TEMP			| EV_PRESS_NEXT			| Seleccionada configurada						| ST_READ_TEMP_CON		| Muestra temperaturas maxima y minima
+	| ST_READ_TEMP_CON		| EV_PRESS_BACK			| 												| ST_READ_TEMP			|
+	| ST_READ_TEMP			| EV_PRESS_NEXT			| Seleccionada historicas						| ST_READ_TEMP_HIS		| Muestra medición más reciente
+	| ST_READ_TEMP_HIS		| EV_PRESS_HASH/ASTK	|												| ST_READ_TEMP_HIS		| Va cambiando las mediciones mostradas
+	| ST_READ_TEMP_HIS		| EV_PRESS_BACK			|												| ST_READ_TEMP			|
+	| ST_READ_HUM			| EV_PRESS_HASH/ASTK	|												| ST_READ_HUM			| Cambiar entre configurada e historicas
+	| ST_READ_HUM			| EV_PRESS_NEXT			| Seleccionada configurada						| ST_READ_HUM_CON		| Muestra humedades maxima y minima
+	| ST_READ_HUM_CON		| EV_PRESS_BACK			| 												| ST_READ_HUM			|
+	| ST_READ_HUM			| EV_PRESS_NEXT			| Seleccionada historicas						| ST_READ_HUM_HIS		| Muestra medición más reciente
+	| ST_READ_HUM_HIS		| EV_PRESS_HASH/ASTK	|												| ST_READ_HUM_HIS		| Va cambiando las mediciones mostradas
+	| ST_READ_HUM_HIS		| EV_PRESS_BACK			|												| ST_READ_HUM			|
+	| ST_READ_LIG			| EV_PRESS_HASH/ASTK	|												| ST_READ_LIG			| Cambiar entre configurada e historicas
+	| ST_READ_LIG			| EV_PRESS_NEXT			| Seleccionada configurada						| ST_READ_LIG_CON		| Muestra luminocidades maxima y minima
+	| ST_READ_LIG_CON		| EV_PRESS_BACK			| 												| ST_READ_LIG			|
+	| ST_READ_LIG			| EV_PRESS_NEXT			| Seleccionada historicas						| ST_READ_LIG_HIS 		| Muestra medición más reciente
+	| ST_READ_LIG_HIS		| EV_PRESS_HASH/ASTK	|												| ST_READ_LIG_HIS		| Va cambiando las mediciones mostradas
+	| ST_READ_LIG_HIS		| EV_PRESS_BACK			| 												| ST_READ_LIG			|
  * 	------------------------+-----------------------+-----------------------+-----------------------+------------------------
  */
 
