7a45da189c4247179f6b1e31921d22d778c5ebf4
Lucas F. Ovalle Olivares <luke.ovalle@gmail.com>
2026-03-21 21:30:50 -0300

    Correcciones de Santaigo

--- app/src/task_keypad.c
+++ app/src/task_keypad.c
@@ -4,9 +4,9 @@
 #include "task_menu.h"
 
 /* ===================== CONFIGURACION ===================== */
-#define TASK_KEYPAD_DEBOUNCE_TICKS   20
+#define TASK_KEYPAD_DEBOUNCE_TICKS   10
 #define TASK_KEYPAD_KEYS_QTY         16
-#define KEYPAD_SCAN_SAMPLES  10
+// #define KEYPAD_SCAN_SAMPLES  10  elimino esto para no hacer un x10 en lecturas ya que tengo filtro con la FSM
 /* ========================================================= */
 
 /* ===================== CONTADORES ======================== */
@@ -22,11 +22,22 @@ typedef enum {
     ST_RISING
 } keypad_state_t;
 
+// Utilizo una sola FSM para toda la botonera, con un array de control para cada tecla, asi no tengo que hacer 16 FSM independientes
+/*
 typedef struct {
     keypad_key_t key;
     keypad_state_t state;
     uint32_t tick;
 } keypad_ctrl_t;
+*/
+
+typedef struct {
+    keypad_key_t key;
+    keypad_state_t state;
+    uint32_t tick;
+} keypad_fsm_t;
+
+static keypad_fsm_t keypad;
 /* ========================================================= */
 
 /* ===================== MAPEO DE TECLAS =================== */
@@ -39,18 +50,24 @@ static const keypad_key_t keypad_key_map[TASK_KEYPAD_KEYS_QTY] = {
 /* ========================================================= */
 
 /* ===================== DATOS PRIVADOS ==================== */
-static keypad_ctrl_t keypad_ctrl[TASK_KEYPAD_KEYS_QTY];
+//static keypad_ctrl_t keypad_ctrl[TASK_KEYPAD_KEYS_QTY];
+static keypad_fsm_t keypad;
 /* ========================================================= */
 
 void task_keypad_init(void *parameters)
 {
     (void) parameters;
-
+    /*
     for (uint8_t i = 0; i < TASK_KEYPAD_KEYS_QTY; i++) {
         keypad_ctrl[i].key   = keypad_key_map[i];
         keypad_ctrl[i].state = ST_UP;
         keypad_ctrl[i].tick  = 0;
     }
+    */
+    keypad.key = KEY_NONE;
+    keypad.state = ST_UP;
+    keypad.tick = 0;
+
 }
 
 void task_keypad_update(void *parameters)
@@ -59,23 +76,65 @@ void task_keypad_update(void *parameters)
 
     /* Botonera matricial clásica unibotón */
     keypad_key_t key_read = keypad_scan();
+
+    /* No es necesario hacer muchas muestas
     for (uint8_t i = 0; i < KEYPAD_SCAN_SAMPLES; i++)
     {
     	keypad_key_t key_read_next = keypad_scan();
     	if (key_read_next == KEY_NONE) break;
     	key_read = key_read_next;
     }
-
-    for (uint8_t i = 0; i < KEYPAD_SCAN_SAMPLES; i++) {
-        keypad_key_t key_read_next = keypad_scan();
-        if (key_read_next == KEY_NONE)
-            break;
-
-        key_read = key_read_next;
-    }
+    */
 
     g_task_keypad_cnt++;
 
+    switch (keypad.state)
+    {
+    case ST_UP:
+        if (key_read != KEY_NONE) {
+            keypad.state = ST_FALLING;
+            keypad.tick = 0;
+            keypad.key = key_read;
+        }
+        break;
+
+    case ST_FALLING:
+        if (key_read == keypad.key) {
+            if (++keypad.tick >= TASK_KEYPAD_DEBOUNCE_TICKS) {
+                keypad.state = ST_DOWN;
+                task_menu_push_event(MENU_EV_KEY_PRESSED, keypad.key);
+            }
+        } else {
+            keypad.state = ST_UP;
+        }
+        break;
+
+    case ST_DOWN:
+        if (key_read != keypad.key) {
+            keypad.state = ST_RISING;
+            keypad.tick = 0;
+        }
+        break;
+
+    case ST_RISING:
+        if (key_read != keypad.key) {
+            if (++keypad.tick >= TASK_KEYPAD_DEBOUNCE_TICKS) {
+                keypad.state = ST_UP;
+                task_menu_push_event(MENU_EV_KEY_RELEASED, keypad.key);
+            }
+        } else {
+            keypad.state = ST_DOWN;
+        }
+        break;
+
+    default: // Por si por algun motivo quedo en un estado invalido, lo reseteo
+        keypad.state = ST_UP;
+        keypad.tick = 0;
+        keypad.key = KEY_NONE;
+        break;
+    }
+    
+    /*
     for (uint8_t i = 0; i < TASK_KEYPAD_KEYS_QTY; i++)
     {
         keypad_ctrl_t *s = &keypad_ctrl[i];
@@ -120,4 +179,5 @@ void task_keypad_update(void *parameters)
                 break;
         }
     }
+    */
 }
